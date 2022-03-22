35441f864a520739cfb47108f235dfd7d09a964f
Renato Bueno <renatobdo@gmail.com>
2022-03-22 19:30:07 -0300

    Update mercado_online.sql

--- Semana6-22_03_2022/mercado_online.sql
+++ Semana6-22_03_2022/mercado_online.sql
@@ -1,3 +1,14 @@
+create database survey;
+use survey;
+create table salary_survey(
+	country varchar(50),
+    years_experience bigint,
+    employment_status varchar(50),
+    job_title varchar(50),
+    is_manager varchar(5), 
+    education_level varchar(50)
+    );
+    
 create schema mercado_online;
 
 use mercado_online;
