50279f8bf384e1265d637fd5e16a7f4513ba3134
Renato Bueno <renatobdo@gmail.com>
2022-04-05 21:14:29 -0300

    Update views.sql

--- Semana7_29_03_2022/views.sql
+++ Semana7_29_03_2022/views.sql
@@ -5,13 +5,15 @@
 #
 #### Exercício - 1
 Create table martian_confidential
-(martian_id int,
+(martian_id int primary key,
 first_name varchar(40),
 last_name varchar(80),
 base_id int,
 super_id int,
 salary decimal(8,2),
-dna_id varchar(30));
+dna_id varchar(30),
+foreign key(base_id) references base (base_id)
+);
 
 
 insert into martian_confidential values
@@ -31,6 +33,8 @@ insert into visitor values (1, 1,'George', 'Ambrose'),
 (2, 2,'Kris', 'Cardenas'),
 (3, 3, 'Priscilla', 'Lane'),
 (4, 4,'Jane', 'Thorton');
+
+alter table visitor add primary key (visitor_id); 
 
 ############################################
 ### Exercício - 4
