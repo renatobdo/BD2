1dc4de4efc2556ee8fc9510aeaf0efc5daece68b
Renato Bueno <renatobdo@gmail.com>
2022-04-12 01:36:08 -0300

    Update views2.sql

--- Semana8/views2.sql
+++ Semana8/views2.sql
@@ -574,3 +574,49 @@ ALTER table terms ENGINE = InnoDB;
 ALTER table vendor_contacts ENGINE = InnoDB;
 ALTER table vendors ENGINE = InnoDB;
 
+
+#### Exercício 9 ###########
+create view open_items as 
+	select v.vendor_name, 
+		i.invoice_id, 
+		i.invoice_total , 
+		(i.invoice_total - i.payment_total - i.credit_total) as "saldo devedor (balance_due)"	
+	from vendors v      
+		inner join 
+	invoices i on 
+		v.vendor_id = i.vendor_id    
+	where (i.invoice_total - i.payment_total - i.credit_total) > 0
+	order by vendor_name;
+
+###### Exercício 10 #########
+select * from open_items where balance_due > 1000;
+
+### Exercício 11
+CREATE OR REPLACE VIEW open_items_summary
+AS
+SELECT vendor_name, COUNT(*) AS open_item_count,
+       SUM(invoice_total - credit_total - payment_total) AS open_item_total
+FROM vendors JOIN invoices
+  ON vendors.vendor_id = invoices.vendor_id
+WHERE invoice_total - credit_total - payment_total > 0
+GROUP BY vendor_name
+ORDER BY open_item_total DESC;
+select * from open_items_summary;
+
+#### Exercício 12
+SELECT *FROM open_items_summary LIMIT 5
+
+## Exercício 13
+CREATE OR REPLACE VIEW vendor_address
+AS
+SELECT vendor_id, vendor_address1, 
+vendor_address2, vendor_city, vendor_state, vendor_zip_code
+FROM vendors;
+select * from vendor_address;
+
+### Exercício 14 ####
+select * from vendor_address where vendor_id = 4;
+UPDATE vendor_address
+SET vendor_address1 = '1990 Westwood Blvd',
+    vendor_address2 = 'Ste 260'
+WHERE vendor_id = 4;
