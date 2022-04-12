8e8c15767b32c8daef40c60e7d5b0f12c6a7bae2
Renato Bueno <renatobdo@gmail.com>
2022-04-12 02:13:48 -0300

    Update table_website_sessions.sql

--- Semana8/table_website_sessions.sql
+++ Semana8/table_website_sessions.sql
@@ -13,3 +13,15 @@ create table website_sessions(
 
 
 ## importe as tabelas 14 e 15 do git https://github.com/renatobdo/BD2/tree/main/Semana6-22_03_2022
+
+##  Exercício 17
+
+create view monthly_sessions as
+select 
+	year(created_at) as ano,
+    month(created_at) as mes,
+    utm_source,
+    utm_campaign, 
+    count(website_session_id) as number_of_sessions
+    from website_sessions
+    group by 1,2,3,4;
