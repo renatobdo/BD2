7f551e2eda11675e4b481f3e8c76cef00e73a2e0
Renato Bueno <renatobdo@gmail.com>
2022-04-17 01:49:38 -0300

    Create handlingErrors.sql

---
+++ Semana9/handlingErrors.sql
@@ -0,0 +1,34 @@
+##
+# Criação da tabela
+##
+CREATE TABLE SupplierProducts (
+    supplierId INT,
+    productId INT,
+    PRIMARY KEY (supplierId , productId)
+);
+###
+# Criando a procedure que irá tratar o erro
+###
+CREATE PROCEDURE InsertSupplierProduct(
+    IN inSupplierId INT, 
+    IN inProductId INT
+)
+BEGIN
+    -- exit if the duplicate key occurs
+    DECLARE EXIT HANDLER FOR 1062
+    BEGIN
+ 	SELECT CONCAT('Duplicate key (',inSupplierId,',',inProductId,') occurred') AS message;
+    END;
+    
+    -- insert a new row into the SupplierProducts
+    INSERT INTO SupplierProducts(supplierId,productId)
+    VALUES(inSupplierId,inProductId);
+    
+    -- return the products supplied by the supplier id
+    SELECT COUNT(*) 
+    FROM SupplierProducts
+    WHERE supplierId = inSupplierId;
+    
+END$$
+
+DELIMITER ;
