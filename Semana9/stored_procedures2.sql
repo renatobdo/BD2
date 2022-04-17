3393e2f35f51d24038a44e667b71f31903769fdd
Renato Bueno <renatobdo@gmail.com>
2022-04-17 02:41:11 -0300

    Update stored_procedures2.sql

--- Semana9/stored_procedures2.sql
+++ Semana9/stored_procedures2.sql
@@ -149,3 +149,65 @@ END$$
 #
 CALL get_order_by_cust(141,@shipped,@canceled,@resolved,@disputed);
 SELECT @shipped,@canceled,@resolved,@disputed;
+#
+#########################################
+# Criando Funções armazenadas
+# https://www.mysqltutorial.org/mysql-stored-function/
+#########################################
+DELIMITER $$
+CREATE FUNCTION CustomerLevel(
+	credit DECIMAL(10,2)
+) 
+RETURNS VARCHAR(20)
+DETERMINISTIC
+BEGIN
+    DECLARE customerLevel VARCHAR(20);
+
+    IF credit > 50000 THEN
+		SET customerLevel = 'PLATINUM';
+    ELSEIF (credit >= 50000 AND 
+			credit <= 10000) THEN
+        SET customerLevel = 'GOLD';
+    ELSEIF credit < 10000 THEN
+        SET customerLevel = 'SILVER';
+    END IF;
+	-- return the customer level
+	RETURN (customerLevel);
+END$$
+DELIMITER ;
+#
+# Chamando a função no comando select
+#
+SELECT 
+    customerName, 
+    CustomerLevel(creditLimit)
+FROM
+    customers
+ORDER BY 
+    customerName;
+#
+########################################################
+# Chamando uma função armazenada em uma stored procedure
+########################################################
+DELIMITER $$
+CREATE PROCEDURE GetCustomerLevel3(
+    IN  customerNo INT,  
+    OUT customerLevel VARCHAR(20)
+)
+BEGIN
+	DECLARE credit DEC(10,2) DEFAULT 0;
+    -- get credit limit of a customer
+    SELECT 
+		creditLimit 
+	INTO credit
+    FROM customers
+    WHERE 
+		customerNumber = customerNo;
+    -- call the function 
+    SET customerLevel = CustomerLevel(credit);
+END$$
+DELIMITER ;
+#
+#####
+CALL GetCustomerLevel3(-131,@customerLevel);
+SELECT @customerLevel;
