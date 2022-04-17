94c3031500f94b3d07cd9a1353493e667a8bc009
Renato Bueno <renatobdo@gmail.com>
2022-04-17 02:24:53 -0300

    Update stored_procedures2.sql

--- Semana9/stored_procedures2.sql
+++ Semana9/stored_procedures2.sql
@@ -95,3 +95,56 @@ DELIMITER ;
 CALL GetDeliveryStatus(10100,@delivery);
 select @delivery as 'status da entrega';
 #
+#
+####################
+# Procedimento armazenado que retorna múltiplos valores
+#####################
+DELIMITER $$
+
+CREATE PROCEDURE get_order_by_cust(
+	IN cust_no INT,
+	OUT shipped INT,
+	OUT canceled INT,
+	OUT resolved INT,
+	OUT disputed INT)
+BEGIN
+		-- shipped
+		SELECT
+            count(*) INTO shipped
+        FROM
+            orders
+        WHERE
+            customerNumber = cust_no
+                AND status = 'Shipped';
+
+		-- canceled
+		SELECT
+            count(*) INTO canceled
+        FROM
+            orders
+        WHERE
+            customerNumber = cust_no
+                AND status = 'Canceled';
+
+		-- resolved
+		SELECT
+            count(*) INTO resolved
+        FROM
+            orders
+        WHERE
+            customerNumber = cust_no
+                AND status = 'Resolved';
+
+		-- disputed
+		SELECT
+            count(*) INTO disputed
+        FROM
+            orders
+        WHERE
+            customerNumber = cust_no
+                AND status = 'Disputed';
+
+END$$
+#
+CALL get_order_by_cust(141,@shipped,@canceled,@resolved,@disputed);
+SELECT @shipped,@canceled,@resolved,@disputed;
