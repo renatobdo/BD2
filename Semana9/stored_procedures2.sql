e460640649fc0a9b234ec67151e817018fcba17f
Renato Bueno <renatobdo@gmail.com>
2022-04-17 00:28:20 -0300

    Update stored_procedures2.sql

--- Semana9/stored_procedures2.sql
+++ Semana9/stored_procedures2.sql
@@ -31,3 +31,32 @@ SELECT @level;
 CALL GetCustomerLevel(447, @level); 
 SELECT @level;
 #
+####################################################################
+# Procedimento armazenado que utiliza CASE
+####################################################################
+#
+DELIMITER $$
+CREATE PROCEDURE GetCustomerShipping(
+	IN  pCustomerNUmber INT, 
+	OUT pShipping       VARCHAR(50))
+BEGIN
+    DECLARE customerCountry VARCHAR(100);
+	SELECT 
+		country
+	INTO customerCountry FROM
+		customers
+	WHERE
+		customerNumber = pCustomerNUmber;
+    CASE customerCountry
+		WHEN  'USA' THEN
+		   SET pShipping = '2-day Shipping';
+		WHEN 'Canada' THEN
+		   SET pShipping = '3-day Shipping';
+		ELSE
+		   SET pShipping = '5-day Shipping';
+	END CASE;
+END$$
+DELIMITER ;
+#
+CALL GetCustomerShipping(112,@shipping);
+SELECT @shipping;
