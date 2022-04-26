18c3e11b10d158162abf80e119227d7d8a0f0d0b
Renato Bueno <renatobdo@gmail.com>
2022-04-26 17:01:31 -0300

    Update transacoes.sql

--- Semana11/transacoes.sql
+++ Semana11/transacoes.sql
@@ -1,3 +1,36 @@
+#######################################
+# savepoint
+######################################
+USE ap;
+
+START TRANSACTION;
+SAVEPOINT before_invoice;
+INSERT INTO
+    invoices 
+        VALUES (115,
+            34,
+            'ZXA-080',
+            '2018-01-18',
+            14092.59, 0, 0, 3, '2018-04-18', NULL);
+
+SAVEPOINT before_line_iteml;
+
+INSERT INTO invoice_line_items
+        VALUES (115, 1, 160, 4447.23, ' HW upgrade ');
+
+SAVEPOINT before_line_item2;
+
+INSERT INTO invoice_line_items
+        VALUES (115, 2, 167, 9645.36,'0S upgrade ');
+
+ROLLBACK TO SAVEPOINT before_line_item2;
+
+ROLLBACK TO SAVEPOINT before_line_iteml;
+
+ROLLBACK TO SAVEPOINT before_invoice;
+
+COMMIT;
+
 ###############################################################
 # Transações
 #https://www.mysqltutorial.org/mysql-transaction.aspx 
