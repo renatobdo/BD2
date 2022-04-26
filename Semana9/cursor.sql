5bfaf66c60043ffc9df56c8159c2d21dc9b01a09
Renato Bueno <renatobdo@gmail.com>
2022-04-26 16:52:50 -0300

    Update cursor.sql

--- Semana9/cursor.sql
+++ Semana9/cursor.sql
@@ -99,3 +99,57 @@ SELECT @emailList;
 
   CALL SomaKilometragem(@variavel_temporaria);
   SELECT @variavel_temporaria;
+  
+  ###########################################
+  # Exemplo 3
+  ########
+##
+# outro exemplo de cursores
+#
+CREATE TABLE tutorials (
+   ID INT PRIMARY KEY,
+   TITLE VARCHAR(100),
+   AUTHOR VARCHAR(40),
+   DATE VARCHAR(40)
+);
+
+insert into tutorials values(1, 'Java', 'Krishna', '2019-09-01');
+insert into tutorials values(2, 'JFreeCharts', 'Satish', '2019-05-01');
+insert into tutorials values(3, 'JavaSprings', 'Amit', '2019-05-01');
+insert into tutorials values(4, 'Android', 'Ram', '2019-03-01');
+insert into tutorials values(5, 'Cassandra', 'Pruthvi', '2019-04-06');
+
+CREATE TABLE backup (
+   ID INT,
+   TITLE VARCHAR(100),
+   AUTHOR VARCHAR(40),
+   DATE VARCHAR(40)
+);
+
+DELIMITER //
+CREATE PROCEDURE ExampleProc()
+   BEGIN
+      DECLARE done INT DEFAULT 0;
+      DECLARE tutorialID INTEGER;
+      DECLARE tutorialTitle, tutorialAuthor, tutorialDate VARCHAR(20);
+      
+      DECLARE cur CURSOR FOR SELECT * FROM tutorials;
+      
+      DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+      
+      OPEN cur;
+      
+      label: LOOP
+		FETCH cur INTO tutorialID, 
+			tutorialTitle, tutorialAuthor, tutorialDate;
+		INSERT INTO backup VALUES(tutorialID, tutorialTitle, tutorialAuthor, tutorialDate);
+		IF done = 1 THEN 
+			LEAVE label;
+		END IF;
+      END LOOP;
+      CLOSE cur;
+   END//
+DELIMITER ;
+
+CALL ExampleProc;
+select * from backup;
