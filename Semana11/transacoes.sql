6f6b4c449b55f715a34fc84659a69d3f875ccb1b
Renato Bueno <renatobdo@gmail.com>
2022-04-26 16:56:59 -0300

    Update transacoes.sql

--- Semana11/transacoes.sql
+++ Semana11/transacoes.sql
@@ -9,35 +9,31 @@
 DELIMITER // 
 CREATE PROCEDURE test() 
 BEGIN 
-DECLARE sql_error TINYINT DEFAULT FALSE;
-DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
-SET
-    sql_error = TRUE;
+    DECLARE sql_error TINYINT DEFAULT FALSE;
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+    SET sql_error = TRUE;
 
 START TRANSACTION;
 
-INSERT INTO
-    invoices
-VALUES
-    (115,34,'ZXA-080','2018-01-18', 14092.59, 0, 0, 3, '2018-04-18', NULL);
+    INSERT INTO
+        invoices
+            VALUES(115,34,'ZXA-080','2018-01-18', 14092.59, 0, 0, 3, '2018-04-18', NULL);
 
-INSERT INTO
-    invoice_ line_ items
-VALUES
-    (115, 1, 160, 4447.23, 'HW upgrade');
+    INSERT INTO
+        invoice_ line_ items
+            VALUES(115, 1, 160, 4447.23, 'HW upgrade');
 
-INSERT INTO
-    invoice_ line_ items
-VALUES
-    (115, 2, 167, 9645.36, 'OS upgrade');
+    INSERT INTO
+        invoice_ line_ items
+            VALUES(115, 2, 167, 9645.36, 'OS upgrade');
 
-IF sql_error = FALSE THEN 
-    COMMIT;
-    SELECT 'The transaction was committed.';
-ELSE 
-    ROLLBACK;
-    SELECT 'The transaction was rolled back.';
-END IF;
+    IF sql_error = FALSE THEN 
+        COMMIT;
+        SELECT 'The transaction was committed.';
+    ELSE 
+        ROLLBACK;
+        SELECT 'The transaction was rolled back.';
+    END IF;
 END //
 
 ######
