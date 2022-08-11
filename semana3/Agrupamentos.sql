9252e10ccf19f1d29ffbbcdd2ab82dc64a058c69
Renato Bueno <renatobdo@gmail.com>
2022-08-10 23:49:48 -0300

    Update Agrupamentos.sql

--- semana3/Agrupamentos.sql
+++ semana3/Agrupamentos.sql
@@ -2,3 +2,37 @@ SELECT    DISTINCT department_id FROM
 Employees
 ORDER BY    
 department_id;
+
+## GROUPING SETS
+CREATE TABLE inventory (
+    warehouse VARCHAR(255),
+    product VARCHAR(255) NOT NULL,
+    model VARCHAR(50) NOT NULL,
+    quantity INT,
+    PRIMARY KEY (warehouse,product,model)
+);
+INSERT INTO inventory(warehouse, product, model, quantity)
+VALUES('San Jose', 'iPhone','6s',100);
+INSERT INTO inventory(warehouse, product, model, quantity)
+VALUES('San Fransisco', 'iPhone','6s',50);
+INSERT INTO inventory(warehouse, product, model, quantity)
+VALUES('San Jose','iPhone','7',50);
+INSERT INTO inventory(warehouse, product, model, quantity)
+VALUES('San Fransisco', 'iPhone','7',10);
+INSERT INTO inventory(warehouse, product, model, quantity)
+VALUES('San Jose','iPhone','X',150);
+INSERT INTO inventory(warehouse, product, model, quantity)
+VALUES('San Fransisco', 'iPhone','X',200);
+INSERT INTO inventory(warehouse, product, model, quantity)
+VALUES('San Jose','Samsung','Galaxy S',200);
+INSERT INTO inventory(warehouse, product, model, quantity)
+VALUES('San Fransisco','Samsung','Galaxy S',200);
+INSERT INTO inventory(warehouse, product, model, quantity)
+VALUES('San Fransisco','Samsung','Note 8',100);
+INSERT INTO inventory(warehouse, product, model, quantity)
+VALUES('San Jose','Samsung','Note 8',150);
+
+SELECT 
+    *
+FROM
+    inventory;
