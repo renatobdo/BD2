8eccacb61c67450171dd707f30c8920727f342f6
Renato Bueno <renatobdo@gmail.com>
2022-09-20 20:08:48 -0300

    Novo banco de dados

---
+++ BancosDeDados/survey.sql
@@ -0,0 +1,9 @@
+create database survey;
+use survey;
+
+create table salary_survey(country varchar(50),
+years_experience bigint,
+employment_status varchar(50),
+job_title varchar(50),
+is_manager varchar(5),
+education_level varchar(50));
