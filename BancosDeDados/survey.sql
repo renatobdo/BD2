9291a41d00a8aa3abf522c3aac09e9c9d14709a2
Renato Bueno <renatobdo@gmail.com>
2022-09-20 21:55:49 -0300

    Novo banco de dados

--- BancosDeDados/survey.sql
+++ BancosDeDados/survey.sql
@@ -7,3 +7,17 @@ employment_status varchar(50),
 job_title varchar(50),
 is_manager varchar(5),
 education_level varchar(50));
+
+select count(*) from salary_survey;
+drop table salary_survey3;
+
+select country,
+    count(*) as respondents,
+    avg(years_experience) as yrs_experience,
+    avg(case when is_manager = 'Yes' then 1 else 0 end) as pct_mgrs,
+    avg(case when education_level = 'Masters' then 1 else 0 end) as pct_masters
+from salary_survey
+group by 1 order by 2 desc;
+
+select * from salary_survey order by years_experience DESC;
+update salary_survey set years_experience = null where years_experience > 60;
--- Semana8/views4.sql
+++ Semana8/views4.sql
@@ -594,3 +594,29 @@ create view open_items as
 
 select * from open_items;
 select * from open_items where balance_due > 1000;
+
+CREATE OR REPLACE VIEW open_items_summary AS
+	SELECT vendor_name, COUNT(*) AS open_item_count,
+	SUM(invoice_total-credit_total-payment_total) AS
+	open_item_total
+	FROM vendors JOIN 
+		invoices ON vendors.vendor_id= invoices.vendor_id
+	WHERE invoice_total-credit_total-payment_total> 0
+	GROUP BY vendor_name
+	ORDER BY open_item_total DESC;
+
+select * from open_items_summary;
+
+select * from open_items_summary LIMIT 5;
+
+CREATE OR REPLACE VIEW vendor_address
+AS
+SELECT vendor_id,
+vendor_address1,
+vendor_address2,
+vendor_city,
+vendor_state,
+vendor_zip_code
+FROM vendors;
+
+select * from vendor_address;
