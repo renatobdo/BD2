f6e6afeebfe9e08ca81a7462ad252e72423957b4
Renato Bueno <renatobdo@gmail.com>
2022-10-11 22:22:51 -0300

    Organização

---
+++ Semana11/niveisIsolamento.sql
@@ -0,0 +1,11 @@
+set autocommit = 0;
+set foreign_key_checks=0;
+
+START TRANSACTION;
+select * from employees where employeeNumber between 1165
+ and 1166;
+
+
+SELECT @@global. tx_isolation;
+set global transaction isolation level serializable;
+set global transaction isolation level REPEATABLE READ;
---
+++ Semana11/niveisIsolamento2.sql
@@ -0,0 +1,10 @@
+set autocommit = 0;
+set foreign_key_checks=0;
+
+START TRANSACTION;
+update employees set firstName = 'Renato' where 
+employeeNumber = 1165;
+commit;
+
+select * from employees where employeeNumber between 1165 and 1166;
+
